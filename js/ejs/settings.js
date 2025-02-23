6039d941cf8283f1f228c485e55bd9a9f40d3f4d
Hisjadenandonly <hisjaden@gmail.com>
2025-02-23 16:05:07 +0100

    settings.js

--- js/ejs/settings.js
+++ js/ejs/settings.js
@@ -1,4 +1,4 @@
 export const service = "service_szpcma8";
 export const template = "template_b16qkvl";
 export const apiKey = "ufNZdy9duBtqahAwW";
-export const recieverMail = "hisjaden@gmail.com";
+export const recieverMail = "fsilomi@gmail.com";
